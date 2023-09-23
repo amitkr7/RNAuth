2703bc966fd220cd0991a5ce99a2f1320330095f
amitkr7 <amit840901@gmail.com>
2023-09-23 20:04:34 +0530

    User login

--- util/auth.js
+++ util/auth.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
 const API_KEY = 'AIzaSyA7qMJNif1yFd582GEpQujzMFkOCa76bAA';
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+};
 
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    { email: email, password: password, returnSecureToken: true }
-  );
+  await authenticate('signUp', email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate('signInWithPassword', email, password);
 };
